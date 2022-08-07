788e6e5435e954b5eb538e82f381b7edcfcb0bc1
Jeremy Pérez <jerespn@gmail.com>
2022-08-07 12:55:59 -0400

    cobus bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_garden.mk
+   $(LOCAL_DIR)/du_garden.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_garden-user \
-    lineage_garden-userdebug \
-    lineage_garden-eng
+    du_garden-user \
+    du_garden-userdebug \
+    du_garden-eng
